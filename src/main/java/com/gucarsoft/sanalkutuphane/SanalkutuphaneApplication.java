6b904077bd0ef348571e8597a46f08e0352ee8a1
Gurkan UCAR <gurkan.ucar@bulutmd.com>
2021-04-12 14:03:43 +0300

    Initial commit

---
+++ src/main/java/com/gucarsoft/sanalkutuphane/SanalkutuphaneApplication.java
@@ -0,0 +1,13 @@
+package com.gucarsoft.sanalkutuphane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanalkutuphaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanalkutuphaneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gucarsoft/sanalkutuphane/SanalkutuphaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucarsoft.sanalkutuphane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SanalkutuphaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
