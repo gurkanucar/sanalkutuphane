253a3d31e05b62cb13e8c85ceb991fc5b2c56e3b
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2021-04-14 15:10:25 +0300

    bug fix

--- src/main/java/com/gucarsoft/sanalkutuphane/config/WebSocketConfig.java
+++ src/main/java/com/gucarsoft/sanalkutuphane/config/WebSocketConfig.java
@@ -2,10 +2,10 @@ package com.gucarsoft.sanalkutuphane.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
